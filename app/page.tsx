16a8b6952990ad15b48c60a1f0120697dfc99d7d
sandie679 <ropafadzotalent48@gmail.com>
2025-08-06 17:24:37 -0700

    changes banner

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function HomePage() {
         </div>
       </div>
 
-      <div className="flex flex-col items-start mt-20 mx-4 md:mx-10">
+      <div className="flex flex-col items-start mt-20 mx-4 md:mx-10 bg-white">
         <h2 className="text-green-950 text-4xl md:text-7xl font-semibold font-roboto">
           ABOUT US
         </h2>
@@ -99,7 +99,9 @@ export default function HomePage() {
         <h2 className="text-3xl font-playfair font-semibold mb-6 ml-2 md:ml-4">
           OUR SPECIALS
         </h2>
+        <div className="bg-white">
         <CardSection />
+        </div>
       </div>
   <div className="mt-10 md:mt-0">
       <Footer  />
